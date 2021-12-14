aa7b48049c7b00f2b3f83c084d0c99a85add021a
Opilane TTHK <opilane@tthk.ee>
2021-12-14 10:10:48 +0200

    Add project files.

---
+++ vigade.otsing.3.py
@@ -0,0 +1,29 @@
+from random import *
+def arvud_loendis():
+    print("Данные:")
+    n=abs(int(input("Сколько целых чисел генерируем в список? => ")))
+    mini=int((input("Введите минимальное число диапазона => "))
+    maxi=int(input("Введите максимальное число диапазона => "))
+    if mini>=maxi:
+        vahetus(mini,maxi):
+    generator(n,s,mini,maxi):
+    print()
+    print("Результаты:")
+    print("Полученный список от",mini,"до",maxi,s)
+    sort(s)
+    print("Отсортированный список",s)
+    jagamine(s,pos,neg,nol):
+    print("Список положительных элементов",pos)
+    print("Список отрицательных элементов",neg)
+    print("Список нулевых элементов",null)
+    kesk=keskmine(pos,n):
+    lisamine(s,kesk):
+    print("Среднее положительных:",kesk)
+    kesk=keskmine(neg,n):
+    lisamine(s,kesk):
+    print("Среднее отрицательных:",kesk)
+    print("Добавляем средние в изначалный массив:")
+    sort(s)
+    print(s)
+arvud_loendis
+
